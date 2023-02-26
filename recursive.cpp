efbd011111936f8efe68e99ea0e88a58faa389a7
Mehak-Fatima18 <125726563+Mehak-Fatima18@users.noreply.github.com>
2023-02-26 14:06:20 +0500

    Add files via upload

---
+++ recursive.cpp
@@ -0,0 +1,49 @@
+// recursive.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<iostream>
+using namespace std;
+
+int power(int num,int n)
+{
+	
+	if (n ==0)
+		return 1;
+	else
+	{
+		return  num * power(num, n-1);
+		
+	}
+}
+int main()
+{
+	int fact;
+	int num,n;
+	cout << "enter a number = " << endl;
+	cin >> num;
+	cout << "enter power= ";
+	cin >> n;
+	if (num < 0)
+	{
+		cout << "number is negative";
+
+	}
+	else
+	{
+		fact = power(num,n);
+		cout << "answer is " << "is = " << fact;
+	}
+	system("pause");
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
